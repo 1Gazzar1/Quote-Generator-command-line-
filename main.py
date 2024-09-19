220d8986e218544a1d96b72bb696f0c2ca0b2162
mhmda <mhmdaljzar036@gmail.com>
2024-09-19 21:51:31 +0300

    addes basic logic

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+import requests 
+
+def get_quote():
+    response = requests.get("https://api.quotable.io/random",verify=False)
+
+    if not response.status_code >= 400 :
+        data = response.json()
+        quote = data["content"]
+        author = data["author"]
+        return f"{quote} \nBy {author}"
+    else : 
+        return f"something went wrong ,response code is {response.status_code}"
+
+while True : 
+    
+    inp = input("do you want a random quote ?(y/n)")
+    if inp == "y" :
+        print(get_quote())
+    elif inp == "n" :
+        break
+    else : 
+        print("Invalid input")
+        break
+
