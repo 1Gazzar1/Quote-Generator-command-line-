02dbc74460c538c2af46c00590d6cddf6af2ef19
mhmda <mhmdaljzar036@gmail.com>
2024-09-20 16:06:24 +0300

    added tweaks to make the quote more visiable

--- main.py
+++ main.py
@@ -15,24 +15,30 @@ def get_quote():
         return f"something went wrong ,response code is {response.status_code}"
 
 
-font = ImageFont.truetype("fonts\\LemonJellyPersonalUse-dEqR.ttf",70)
-text_color = (0,0,0)
+font = ImageFont.truetype("fonts\\Play-Bold.ttf",40)
+text_color = (255,255,255)
 
 while True : 
     
     inp = input("do you want a random quote ?(y/n)").strip().lower()
     if inp == "y" :
         # loading the image
-        image = Image.open(BytesIO(Generate_random_image_without_path(blur= 2)))
-        text_pos = (300,image.size[1]//2-50)
-
+        image = Image.open(BytesIO(Generate_random_image_without_path(blur= 5))).convert("RGBA")
+        
         # loading the quote and author 
         quote , author = get_quote()
-
+        full_quote = f"{quote}\nBy {author}"
         # drawing the text to the image
         draw = ImageDraw.Draw(image)
         
-        draw.multiline_text(text_pos,f"{quote}\nBy{author}",text_color,font= font)
+        text_pos = (50,50)
+        padding = 10
+
+        box_pos = draw.textbbox(text_pos,full_quote,font)
+         
+        box = (box_pos[0] - padding,  box_pos[1] - padding, box_pos[2] + padding, box_pos[3] + padding)
+        draw.rectangle(box, fill=(0, 0, 0,128))
+        draw.multiline_text(text_pos,full_quote,text_color,font= font)
         
         image.show()
     elif inp == "n" :
