324b87a0cb3b986ca9421f8c1b878b9d4b417107
mhmda <mhmdaljzar036@gmail.com>
2024-09-20 15:05:42 +0300

    added logic to draw text to an image

--- image_generator.py
+++ image_generator.py
@@ -32,4 +32,34 @@ def Generate_random_customizable_image(file_path : str ,
     else : 
         print(f"process failed , status code = {response.status_code}")
 
+def Generate_random_image_without_path(
+                                        width : int = 1920 , height : int = 1080 ,
+                                        blur : int = False , grayscale : bool = False ,
+                                        img_id : int = None):
+    # Cofiguring the Url as intended for the user 
+    if img_id == None :
+        url = f"https://picsum.photos/{width}/{height}/"
+    else : 
+        url = f"https://picsum.photos/id/{img_id}/{width}/{height}/"
+
+    params = []
+    if blur :
+         params.append(f"blur={blur}")
+    if grayscale :
+        params.append(f"grayscale")
+
+    url += "?" + "&".join(params)
+
+    # making the request from the api 
+    response = requests.get(url)
+
+    # generating the image
+    if response.status_code == 200 :
+
+        content = response.content 
+        
+        return content
+
+    else : 
+        print(f"process failed , status code = {response.status_code}")
 #Generate_random_customizable_image("Data\\image_2.jpg",1360,720,blur=0,img_id=667,grayscale=True)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests 
+from image_generator import Generate_random_image_without_path
+from PIL import Image,ImageFont,ImageDraw
+from io import BytesIO
 
 def get_quote():
     response = requests.get("https://api.quotable.io/random",verify=False)
@@ -7,15 +10,31 @@ def get_quote():
         data = response.json()
         quote = data["content"]
         author = data["author"]
-        return f"{quote} \nBy {author}"
+        return quote,author
     else : 
         return f"something went wrong ,response code is {response.status_code}"
+
+
+font = ImageFont.truetype("fonts\\LemonJellyPersonalUse-dEqR.ttf",70)
+text_color = (0,0,0)
 
 while True : 
     
     inp = input("do you want a random quote ?(y/n)").strip().lower()
     if inp == "y" :
-        print(get_quote())
+        # loading the image
+        image = Image.open(BytesIO(Generate_random_image_without_path(blur= 2)))
+        text_pos = (300,image.size[1]//2-50)
+
+        # loading the quote and author 
+        quote , author = get_quote()
+
+        # drawing the text to the image
+        draw = ImageDraw.Draw(image)
+        
+        draw.multiline_text(text_pos,f"{quote}\nBy{author}",text_color,font= font)
+        
+        image.show()
     elif inp == "n" :
         break
     else : 
